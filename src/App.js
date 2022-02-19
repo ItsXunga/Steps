41c68222eef354c378111e7e63e80881162288d2
Beatriz <beatrizso1@hotmail.com>
2022-02-19 22:50:18 +0000

    categorias 1st page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Landing from './components/Landing';
-import Login from './components/Login';
+import { Route, Routes } from "react-router-dom"
+import "./App.css"
+import Landing from "./components/Landing"
+import Login from "./components/Login"
+import Categorias from "./components/categorias/categorias"
 
 function App() {
   return (
-      <Routes>
-        <Route  path='/' element={<Landing />}/>
-        <Route path='/login' element={<Login />}/>
-      </Routes>
-  );
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/categorias" element={<Categorias />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,8 @@
-import React from "react";
-import { theme } from "../assets/gradient";
+import React from "react"
+import { theme } from "../assets/gradient"
 
 const Login = () => {
-    return(
-        <div style={theme}>
-            Login
-        </div>
-    )
+  return <div style={theme}>Login</div>
 }
 
-export default Login;
+export default Login
