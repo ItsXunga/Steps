6ee468482943c1ef4bee12e5760066948b3f73b3
Ivo Martins <ivodmartins@ua.pt>
2022-03-03 00:42:38 +0000

    separação

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import menu_categorias from "../assets/img/menu_categorias.svg";
 import menu_add_route from "../assets/img/menu_add_route.svg";
 import menu_perfil from "../assets/img/menu_perfil.svg";
@@ -7,31 +7,29 @@ import menuClosed from "../assets/img/menu/hamburguerClosed.svg";
 import menuOpened from "../assets/img/menu/hamburguerOpened.svg";
 
 const Nav = () => {
+  const [modoRota, setModoRota] = useState(false)
   return (
     <>
-    <   Link to="/categorias">
-          <img
-            style={{ margin: "0rem .2rem" }}
-            alt='categorias'
-            src={menu_categorias}
-          />
-        </Link>
-        <Link to="/">
-          <img
-            style={{ margin: "0rem .2rem" }}
-            alt='nova rota'
-            src={menu_add_route}
-          />
-        </Link>
-        <Link to="/profile">
-          <img style={{ margin: "0rem .2rem" }}
-          alt='perfil' 
-          src={menu_perfil} />
-        </Link>
-        <img style={{ margin: "0rem .2rem" }}
-        alt='menu'
-        src={menuOpened} /></>
-  )
-}
+      <Link to="/categorias">
+        <img
+          style={{ margin: "0rem .2rem" }}
+          alt="categorias"
+          src={menu_categorias}
+        />
+      </Link>
+      <Link to="/main" >
+        <img
+          style={{ margin: "0rem .2rem" }}
+          alt="nova rota"
+          src={menu_add_route}
+        />
+      </Link>
+      <Link to="/profile">
+        <img style={{ margin: "0rem .2rem" }} alt="perfil" src={menu_perfil} />
+      </Link>
+      <img style={{ margin: "0rem .2rem" }} alt="menu" src={menuOpened} />
+    </>
+  );
+};
 
-export default Nav
+export default Nav;
