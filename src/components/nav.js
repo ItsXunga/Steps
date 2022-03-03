2244803dfa219bdd996a34fd60e3807c8c6053c7
Ivo Martins <ivodmartins@ua.pt>
2022-03-03 22:52:43 +0000

    animação menu

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,11 +5,21 @@ import menu_add_route from "../assets/img/menu_add_route.svg";
 import menu_perfil from "../assets/img/menu_perfil.svg";
 import menuClosed from "../assets/img/menu/hamburguerClosed.svg";
 import menuOpened from "../assets/img/menu/hamburguerOpened.svg";
+import { motion } from "framer-motion";
 
 const Nav = (props) => {
   const [modoRota, setModoRota] = useState(false);
+  const [menuImage, setMenuImg] = useState(menuClosed);
 
   props.checkModo(modoRota);
+
+  function openMenu() {
+    if (menuImage === menuClosed) {
+      setMenuImg(menuOpened);
+    } else {
+      setMenuImg(menuClosed);
+    }
+  }
 
   const changeModoRota = () => {
     if (modoRota === false) {
@@ -21,25 +31,211 @@ const Nav = (props) => {
 
   return (
     <>
-      <Link to="/categorias">
+      {menuImage === menuClosed ? (
+        <Link style={{ display: "none" }} to="/categorias">
+          {menuImage === menuClosed ? (
+            <motion.img
+              animate={{
+                x: 200,
+                transition: {
+                  duration: 0.5,
+                },
+              }}
+              style={{ margin: "0rem .2rem", zIndex: 2 }}
+              alt="categorias"
+              src={menu_categorias}
+            />
+          ) : (
+            <motion.img
+              animate={{
+                x: 0,
+                transition: {
+                  duration: 0.5,
+                },
+              }}
+              style={{ margin: "0rem .2rem", zIndex: 2 }}
+              alt="categorias"
+              src={menu_categorias}
+            />
+          )}
+        </Link>
+      ) : (
+        <Link to="/categorias">
+          {menuImage === menuClosed ? (
+            <motion.img
+              animate={{
+                x: 200,
+                transition: {
+                  duration: 0.5,
+                },
+              }}
+              style={{ margin: "0rem .2rem", zIndex: 2 }}
+              alt="categorias"
+              src={menu_categorias}
+            />
+          ) : (
+            <motion.img
+              animate={{
+                x: 0,
+                transition: {
+                  duration: 0.5,
+                },
+              }}
+              style={{ margin: "0rem .2rem", zIndex: 2 }}
+              alt="categorias"
+              src={menu_categorias}
+            />
+          )}
+        </Link>
+      )}
+
+      {menuImage === menuClosed ? (
+        <Link style={{ display: "none" }} to="/main">
+          {menuImage === menuClosed ? (
+            <motion.img
+              animate={{
+                x: 135,
+                transition: {
+                  duration: 0.5,
+                },
+              }}
+              style={{ margin: "0rem .2rem", zIndex: 3 }}
+              alt="nova rota"
+              src={menu_add_route}
+              onClick={() => changeModoRota()}
+              className="testeCursor"
+            />
+          ) : (
+            <motion.img
+              animate={{
+                x: 0,
+                transition: {
+                  duration: 0.5,
+                },
+              }}
+              style={{ margin: "0rem .2rem", zIndex: 3 }}
+              alt="nova rota"
+              src={menu_add_route}
+              onClick={() => changeModoRota()}
+            />
+          )}
+        </Link>
+      ) : (
+        <Link to="/main">
+          {menuImage === menuClosed ? (
+            <motion.img
+              animate={{
+                x: 135,
+                transition: {
+                  duration: 0.5,
+                },
+              }}
+              style={{ margin: "0rem .2rem", zIndex: 3 }}
+              alt="nova rota"
+              src={menu_add_route}
+              onClick={() => changeModoRota()}
+              className="testeCursor"
+            />
+          ) : (
+            <motion.img
+              animate={{
+                x: 0,
+                transition: {
+                  duration: 0.5,
+                },
+              }}
+              style={{ margin: "0rem .2rem", zIndex: 3 }}
+              alt="nova rota"
+              src={menu_add_route}
+              onClick={() => changeModoRota()}
+            />
+          )}
+        </Link>
+      )}
+
+      {menuImage === menuClosed ? (
+        <Link style={{ display: "none" }} to="/profile">
+          {menuImage === menuClosed ? (
+            <motion.img
+              animate={{
+                x: 70,
+                transition: {
+                  duration: 0.5,
+                },
+              }}
+              style={{ margin: "0rem .2rem", zIndex: 4 }}
+              alt="perfil"
+              src={menu_perfil}
+            />
+          ) : (
+            <motion.img
+              animate={{
+                x: 0,
+                transition: {
+                  duration: 0.5,
+                },
+              }}
+              style={{ margin: "0rem .2rem", zIndex: 4 }}
+              alt="perfil"
+              src={menu_perfil}
+            />
+          )}
+        </Link>
+      ) : (
+        <Link to="/profile">
+          {menuImage === menuClosed ? (
+            <motion.img
+              animate={{
+                x: 70,
+                transition: {
+                  duration: 0.5,
+                },
+              }}
+              style={{ margin: "0rem .2rem", zIndex: 4 }}
+              alt="perfil"
+              src={menu_perfil}
+            />
+          ) : (
+            <motion.img
+              animate={{
+                x: 0,
+                transition: {
+                  duration: 0.5,
+                },
+              }}
+              style={{ margin: "0rem .2rem", zIndex: 4 }}
+              alt="perfil"
+              src={menu_perfil}
+            />
+          )}
+        </Link>
+      )}
+
+      {menuImage === menuClosed ? (
         <img
-          style={{ margin: "0rem .2rem" }}
-          alt="categorias"
-          src={menu_categorias}
+          style={{
+            margin: "0.3rem .2rem",
+            zIndex: 5,
+            boxShadow: "0px 2px 2px rgba(0, 0, 0, 0.25)",
+            borderRadius: "50%",
+          }}
+          onClick={openMenu}
+          alt="menu"
+          src={menuImage}
         />
-      </Link>
-
-      <img
-        style={{ margin: "0rem .2rem" }}
-        alt="nova rota"
-        src={menu_add_route}
-        onClick={() => changeModoRota()}
-      />
-
-      <Link to="/profile">
-        <img style={{ margin: "0rem .2rem" }} alt="perfil" src={menu_perfil} />
-      </Link>
-      <img style={{ margin: "0rem .2rem" }} alt="menu" src={menuOpened} />
+      ) : (
+        <img
+          style={{
+            margin: "0rem .2rem",
+            zIndex: 5,
+            boxShadow: "0px 2px 2px rgba(0, 0, 0, 0.25)",
+            borderRadius: "50%",
+          }}
+          onClick={openMenu}
+          alt="menu"
+          src={menuImage}
+        />
+      )}
     </>
   );
 };
