c0f40c6fb23521fdd620cca24267ff40e9bfb7a7
beatriz <beatrizso1@hotmail.com>
2022-03-03 15:50:01 +0000

    modais finais

---
+++ src/components/ModalAddRota.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react"
+import Modal from "react-modal"
+import { modalStyles } from "../style/modal_styles"
+
+function ModalAddRota(params) {
+  const [modalIsOpen, setIsOpen] = useState(false)
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={modalStyles}
+        contentLabel="Example Modal"
+      >
+        <div style={{ display: "flex" }}>
+          <div>
+            <img src={require("../assets/img/pin.png")} alt="pin" />
+            <p
+              style={{
+                padding: "1rem 2rem",
+                fontSize: "18px",
+                fontFamily: "ManropeRegular",
+              }}
+            >
+              A tua rota foi adicionada com sucesso!
+            </p>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  )
+}
+
+export default ModalAddRota
