599b632590b1751588bfdacebb239006ec1c7298
Ivo Martins <ivodmartins@ua.pt>
2022-05-25 11:02:33 +0100

    cenas

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const db = require("./utils/database");
 const mongoose = require("mongoose");
 const Routes = require("./routes");
+const cookieParser = require("cookie-parser");
 
 const port = process.env.PORT | 3000;
 
@@ -14,9 +15,18 @@ app.use(express.json());
 app.use("/circuits", Routes.CircuitRoutes);
 app.use("/points", Routes.PointRoutes);
 app.use("/users", Routes.UserRoutes);
+app.use(cookieParser());
+
+// cookies
+app.get("/set-cookies", (req, res) => {
+  res.cookie("newUser", true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true});
+
+  res.send("Cookies working!");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+  console.log('Cookies', req.cookies);
 });
 
 async function main() {
