ed75fcb2388c8b2b3bf7e533329f67a65afac24f
Martin42 <martinarmada@ua.pt>
2022-07-18 22:12:38 +0100

    remove modal Id reducer

--- steps/src/components/redux/store.js
+++ steps/src/components/redux/store.js
@@ -4,7 +4,6 @@ import routeIDReducer from "./routeID";
 import singleRouteReducer from "./singleRouteState";
 import pinSliceReducer from "./pinStorage";
 import modalStateReducer from "./modalState";
-import ModalInfoReducer from "./ReduxModalInfo";
 
 export const store = configureStore({
   reducer: {
@@ -13,6 +12,5 @@ export const store = configureStore({
     routeID: routeIDReducer,
     pinStorage: pinSliceReducer,
     modalState: modalStateReducer,
-    modalId: ModalInfoReducer,
   },
 });
